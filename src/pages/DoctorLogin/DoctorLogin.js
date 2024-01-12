28204fe54dae4b75d8767163387ea1fce99417d1
ShaneCoelho <shanecoelho1@gmail.com>
2024-01-12 19:38:49 +0530

    created the directory structure

---
+++ src/pages/DoctorLogin/DoctorLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorLogin=()=>{
+    return(
+        <div>
+            <h1>This is a doctor login page</h1>
+        </div>
+    )
+}
+
+export default DoctorLogin;
