76c81d793e64b0129d669f270da4e48edea37518
Ynez Dias <ynezdias0501@gmail.com>
2024-01-16 21:31:04 +0530

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DoctorLogin from './pages/DoctorLogin/DoctorLogin';
 import HomePage from './pages/HomePage/HomePage';
+import HomePage from '/pages/HomePage/H'
+import PatientLogin from './pages/PatientLogin';
+import PatientSignup from './pages/PatientSignup';
+import AdminLogin from './pages/AdminLogin';
+
 
 function App() {
   return (
@@ -8,9 +13,15 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/doctorlogin" element={<DoctorLogin />} />
+        <Route path="/patient-login" component={PatientLogin}/>
+        <Route path="/patient-signup" component={PatientSignup} />
+        <Route path="/admin-login" component={AdminLogin} />
+
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,36 @@
-import React from "react";
+// src/components/Home.js
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const HomePage=()=>{
-    return(
-        <div>
-            <h1>This is a home page</h1>
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the MedEase </h1>
+
+      <div className="module-container">
+        <div className="module">
+          <h2>Patient Module</h2>
+          <p>Login or sign up as a patient to manage your health records.</p>
+          <Link to="/patient-login">Patient Login</Link>
+          <Link to="/patient-signup">Patient Signup</Link>
         </div>
-    )
+
+        <div className="module">
+          <h2>Admin Module</h2>
+          <p>Login as an admin to manage the overall application.</p>
+          <Link to="/admin-login">Admin Login</Link>
+        </div>
+
+        <div className="module">
+          <h2>Doctor Module</h2>
+          <p>Login as a doctor to access patient information.</p>
+          <Link to="/doctor-login">Doctor Login</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage;
+export default Home;
+
