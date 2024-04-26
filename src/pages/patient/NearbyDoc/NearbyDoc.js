2bb00ce71cf9cccbdcc99074a5f100b70bd41de6
ShaneCoelho <shanecoelho1@gmail.com>
2024-04-26 19:53:55 +0530

    connected the find location button in Nearby doc to backend and added fields to doctor details

--- src/pages/patient/NearbyDoc/NearbyDoc.js
+++ src/pages/patient/NearbyDoc/NearbyDoc.js
@@ -53,9 +53,9 @@ const NearbyDoc = () => {
     navigate('/patient-appoint', { state: { id: id, name: name, Avatar: Avatar } });
   };
 
-  const handleFindDoc = (id,name,Avatar) => {
+  const handleFindDoc = (google_location) => {
     // Redirect to the "/doctor-appoint" page
-    openLinkInNewTab('https://www.google.com/maps/place/Cardinal+Gracias+Memorial+Hospital/@19.364336,72.8025571,17z/data=!3m1!4b1!4m6!3m5!1s0x3be7aec1a0555555:0x9f68d11dfadfcc39!8m2!3d19.364336!4d72.805132!16s%2Fg%2F1tcwk0fm?authuser=0&entry=ttu');
+    openLinkInNewTab(google_location);
   };
 
   return (
@@ -88,7 +88,7 @@ const NearbyDoc = () => {
                     <p>Qualification: {doctor.qualification}</p>
                     <p>Location: {doctor.city}</p>
                     <button className="book-appointment-btn" onClick={() =>handleBookAppointment(doctor._id,doctor.name,doctor.Avatar)}>Book Appointment</button>
-                    <button className="book-appointment-btn" onClick={() =>handleFindDoc(doctor._id,doctor.name,doctor.Avatar)}>Find Location</button>
+                    <button className="book-appointment-btn" onClick={() =>handleFindDoc(doctor.google_location)}>Find Location</button>
                   </div>
                 </div>
               ))}
