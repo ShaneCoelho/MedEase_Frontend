abeb2927d853d31748d58e27900987614d02c520
Dewain76 <diagodewain5@student.sfit.ac.in>
2024-04-29 18:30:22 +0530

    dashboard done

--- src/pages/doctor/DoctroDash/DocDash.js
+++ src/pages/doctor/DoctroDash/DocDash.js
@@ -7,14 +7,15 @@ import ViewPastAppoint from './Past appointments/ViewPastAppoint';
 import CancelAppoint from './Cancel appoinment/CancelAppoint';
 import StyleDoc2 from './StyleDoc2';
 
+
 const DocDash = ({ open, setOpen, isLoggedOut, data, avatar, content }) => {
   const [selectedOption, setSelectedOption] = useState('');
-
   const navigate = useNavigate();
 
   const handleOptionChange = (option) => {
     setSelectedOption(option);
   };
+
   const handleButtonClick = (item) => {
     // Handle button click based on the item
     console.log(`Button clicked for ${item}`);
@@ -45,10 +46,6 @@ const DocDash = ({ open, setOpen, isLoggedOut, data, avatar, content }) => {
               </Button>
             </li>
           </ul>
-          <nav className="navbar">
-            
-          
-          </nav>
         </nav>
 
         <Drawer
@@ -61,68 +58,83 @@ const DocDash = ({ open, setOpen, isLoggedOut, data, avatar, content }) => {
         >
           {/* Drawer content */}
         </Drawer>
-        <StyleDoc2>
-        <section className="why-us mt-5 mt-md-0" >
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-4 d-flex align-items-stretch">
-                <div className="content">
-                  <h3>Today's Appointments</h3>
-                  <NavLink to="/viewtodayappoint">
-                    <Button onClick={() => handleButtonClick("Today's Appointments")}>
-                      View All
-                    </Button>
-                  </NavLink>
-                </div>
-              </div>
-              <div className="col-lg-8 d-flex align-items-stretch">
-                <div className="icon-boxes d-flex flex-column justify-content-center">
-                  <div className="row">
-                    <div className="col-xl-4 d-flex align-items-stretch">
-                      <div className="icon-box mt-4 mt-xl-0">
-                        <div className="icon" />
-                        <h4>Past Appointments</h4>
-                        <NavLink to="/viewpastappoint">
-                        <Button className="btn" onClick={() => handleButtonClick("Past Appointments")}>
-                    View All
-                  </Button>
-                  </NavLink>
-                      </div>
-                    </div>
-                    <div className="col-xl-4 d-flex align-items-stretch">
-                      <div className="icon-box mt-4 mt-xl-0">
-                        <div className="icon" />
-                        <h4>Cancel Appointments</h4>
-                        <NavLink to="/cancelappoint">
-                        <Button className="btn" onClick={() => handleButtonClick("Cancel Appointments")}>
-                    View All
-                  </Button>
-                  </NavLink>
 
+        <StyleDoc2>
+          <section className="why-us mt-5 mt-md-3">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-4 d-flex align-items-stretch">
+                  <div className="content">
+                    <h3>Today's Appointments</h3>
+                    <p>See who you're meeting today! Check this section to view all your appointments scheduled for the current day. It's a quick way to stay organized and ensure you're ready for each appointment on time.</p>
+                    <NavLink to="/viewtodayappoint">
+                      <Button onClick={() => handleButtonClick("Today's Appointments")}>
+                        View All
+                      </Button>
+                    </NavLink>
+                  </div>
+                </div>
+                <div className="col-lg-8 d-flex align-items-stretch">
+                  <div className="icon-boxes d-flex flex-column justify-content-center">
+                    <div className="row">
+                      <div className="col-xl-4 d-flex align-items-stretch">
+                        <div className="icon-box mt-4 mt-xl-0">
+                          <div className="icon" />
+                          <h4>Past <br></br>Appointments</h4>
+                          <p> Reflect on your medical history. Here, you can find details of your previous appointments, including dates, times. It's helpful for recalling past discussions and tracking progress.</p>
+                          <NavLink to="/viewpastappoint">
+                            <Button className="btn" onClick={() => handleButtonClick("Past Appointments")}>
+                              View All
+                            </Button>
+                          </NavLink>
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-xl-4 d-flex align-items-stretch">
-                      <div className="icon-box mt-4 mt-xl-0">
-                        <div className="icon" />
-                        <h4>Reviews</h4>
-                        <NavLink to="/viewreview">
-                        <Button className="btn2" onClick={() => handleButtonClick("Reviwes")}>
-                    View All
-                  </Button>
-                  </NavLink>
+                      <div className="col-xl-4 d-flex align-items-stretch">
+                        <div className="icon-box mt-4 mt-xl-0">
+                          <div className="icon" />
+                          <h4>Cancel <br></br>Appointments</h4>
+                          <p>Need to reschedule? This area lets you cancel your upcoming appointments. If you can't take an appointment, you can easily cancel it here, ensuring smooth communication with your patients.</p>
+                          <NavLink to="/cancelappoint">
+                            <Button className="btn" onClick={() => handleButtonClick("Cancel Appointments")}>
+                              View All
+                            </Button>
+                          </NavLink>
+                        </div>
+                      </div>
+                      <div className="col-xl-4 d-flex align-items-stretch">
+                        <div className="icon-box mt-4 mt-xl-0">
+                          <div className="icon" />
+                          <h4>Appointment <br></br>Requests</h4>
+                          <p>All Appointment requests! If you need to see your appointments, you can check all appointment requests in this section. Provide details like preferred dates and reasons for the visit, and confirm the appointment.</p>
+                          <NavLink to="/doctor-appoint">
+                            <Button className="btn2" onClick={() => handleButtonClick("Appointment Requests")}>
+                              View All
+                            </Button>
+                          </NavLink>
+                        </div>
+                      </div>
+                      <div className="col-xl-4 d-flex align-items-stretch">
+                        <div className="icon-box mt-4 mt-xl-0">
+                          <div className="icon" />
+                          <h4>Reviews</h4>
+                          <p>How was your patient's experience! After each appointment, you have the opportunity to check feedback</p>
+                          <NavLink to="/viewreview">
+                            <Button className="btn" onClick={() => handleButtonClick("Reviews")}>
+                              View All
+                            </Button>
+                          </NavLink>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className='container2'>
-          <h><i>Welcome Doctor</i></h>
-
-          </div>
-        </section>
-      </StyleDoc2>
+            <div className='container2'>
+              <h><i>Welcome Doctor</i></h>
+            </div>
+          </section>
+        </StyleDoc2>
       </StyleHeader>
 
       {/* Render the appropriate component based on the selected option */}
--- src/pages/doctor/DoctroDash/StyleDoc2.js
+++ src/pages/doctor/DoctroDash/StyleDoc2.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
-import img from '../../../images/book.jpg';
+import img from '../../../images/pattern.jpg';
 
  
 const StyleDoc2 = styled.section`
-background-image: url(${img});
-  background-size: cover;
+ background-image:url(${img});
   background-position: center;
-  height:700px;
+  height:1000px;
 
 
 .why-us .content {
@@ -14,12 +13,14 @@ background-image: url(${img});
   background: #1977cc;
   border-radius: 4px;
   margin-top:200px;
+  margin-right:30px;
   color: #fff;
 }
 
 .why-us .content h3 {
   font-weight: 700;
   font-size: 34px;
+  margin-left:20px;
   margin-bottom: 30px;
   color: #fff;
 }
@@ -29,8 +30,9 @@ background-image: url(${img});
   border-radius: 10px;
   background: #fff;
   box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);
-  padding: 70px 40px;
+  padding: 70px 4px;
   width: 100%;
+  height:90%;
 }
 
 .navbtn{
@@ -50,7 +52,7 @@ background-image: url(${img});
 .btn2 {
   background-color: #1977cc;
   color: #fff;
-  margin-top:35px;
+  margin-top:5px;
   border-radius:25px;
   height:40px;
   width:100px;
@@ -59,12 +61,14 @@ background-image: url(${img});
   font-size: 40px;
   color: #1977cc;
   margin-bottom: 30px;
+  margin-left:1px;
+  margin-top:-60px;
+  margin-right:1px;
 }
 
 .why-us .icon-boxes .icon-box h4 {
   font-size: 20px;
   font-weight: 700;
-  margin: 0 0 30px 0;
   color: #2c4964;
 }
 
@@ -73,11 +77,42 @@ background-image: url(${img});
   color: #848484;
 }
 .container2{
-  color:white;
-  margin-top:60px;
-  margin-left:700px;
+  color:#1977cc;
+  margin-top:-200px;
+  margin-left:880px;
   font-size:60px;
   font-weight:700;
+}
+@media screen and (max-width: 768px) {
+  .why-us .content {
+    margin-top: 100px;
+    margin-right: 0;
+    margin-left: 0px;
+  }
+
+  .container2 {
+    margin-left: 50%;
+    transform: translateX(-50%);
+    display:none;
+    font-size: 30px;
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .why-us .content h3 {
+    font-size: 24px;
+  }
+
+  .btn,
+  .btn2 {
+    width: 80px;
+  }
+
+  .container2 {
+    margin-top: -150px;
+    display:none;
+    font-size: 20px;
+  }
 }
 `;
 
