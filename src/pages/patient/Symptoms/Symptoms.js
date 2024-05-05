1928f4344fd26659029edccbabc31ba208677291
ShaneCoelho <shanecoelho1@gmail.com>
2024-05-05 14:02:24 +0530

    connected symptoms page to deployed backend

--- src/pages/patient/Symptoms/Symptoms.js
+++ src/pages/patient/Symptoms/Symptoms.js
@@ -39,7 +39,7 @@ const Symptoms = () => {
     const handleSubmit = async () => {
 
         try {
-            const response = await axios.post('http://127.0.0.1:5000/predict', {
+            const response = await axios.post('https://medeasediseaseprediction.onrender.com/predict', {
                 symptoms: symptoms
             });
             const { prediction, specialization } = response.data;
